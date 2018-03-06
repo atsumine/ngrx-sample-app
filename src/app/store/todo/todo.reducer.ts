db0858c310763d2304174a6b4177ef9735771e20
atsumine <atsumine.kondo@nextbeat.net>
2018-03-06 10:27:42 +0900

    add reducer

--- src/app/store/todo/todo.reducer.ts
+++ src/app/store/todo/todo.reducer.ts
@@ -19,5 +19,36 @@ export function TodoReducer(state = defaultState, action: Action) {
   console.log(state, action)
 
   switch (action.type) {
+  case TodoActions.GET_TODOS: {
+    return { ...state, loaded: false, loasing: true }
+  }
+  case TodoActions.GET_TODOS_SUCCESS: {
+    return {
+        ...state,
+      todos: [
+          ...action.payload,
+        defaultTodoStates[0]
+      ],
+      loading: false
+    };
+  }
+
+  case TodoActions.DELETE_TODO: {
+    return { ...state, ...state.todos.splice(state.todos.indexOf(action.payload), 1) };
+  }
+
+  case TodoActions.DELETE_TODO_SUCCESS: {
+    return state
+  }
+
+  case TodoActions.DELETE_TODO_ERROR: {
+    return {
+        ...state,
+      todos: [
+          ...state.todos,
+        action.payload
+      ]
+    }
+  }
   }
 }
